fba7383feee85ee255ea5aeecc236a2ac544a57d
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2020-11-20 18:13:16 +0200

    Initial commit

---
+++ Advanced level/Advanced OOP/01. Defining Classes - Lab +Ex/01. Rhombus of Stars.py
@@ -0,0 +1,20 @@
+
+
+def print_rhombus(n):
+    growing = 1
+    shrinking = -1
+
+    # fn- fn -fn -closure
+    def print_line(i, direction):
+        if i == 0:
+            return
+        line = ' ' * (n - i) + "* " * i
+        print(line.rstrip())
+        if i == n:
+            direction = shrinking
+        print_line(i + direction, direction)
+
+    print_line(1, growing)
+
+
+print_rhombus(int(input()))
