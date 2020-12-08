c19b93d5a8a27a94d60e2e22b074d453ac072e39
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2020-12-08 20:25:42 +0200

    initial commit

--- Advanced level/Advanced OOP/06. Polymorphism and Magic Methods - Lab + Ex/06.Groups.py
+++ Advanced level/Advanced OOP/06. Polymorphism and Magic Methods - Lab + Ex/06.Groups.py
@@ -1,3 +1,5 @@
+
+
 class Person:
 
     def __init__(self, name, surname):
@@ -9,6 +11,7 @@ class Person:
 
     def __repr__(self):
         return self.name + ' ' + self.surname
+
 
 class Group:
 
@@ -30,24 +33,26 @@ class Group:
         return f"Person {key}: {self.people[key]}"
 
 
-p0 = Person('Aliko', 'Dangote')
-p1 = Person('Bill', 'Gates')
-p2 = Person('Warren', 'Buffet')
-p3 = Person('Elon', 'Musk')
-p4 = p2 + p3
+# p0 = Person('Aliko', 'Dangote')
+# p1 = Person('Bill', 'Gates')
+# p2 = Person('Warren', 'Buffet')
+# p3 = Person('Elon', 'Musk')
+# p4 = p2 + p3
+#
+# first_group = Group('__VIP__', [p0, p1, p2])
+# second_group = Group('Special', [p3, p4])
+# third_group = first_group + second_group
+#
+# # print(len(first_group))
+# # print(second_group)
+# # print(third_group[0])
+#
+# for person in third_group:
+#     print(person)
 
-first_group = Group('__VIP__', [p0, p1, p2])
-second_group = Group('Special', [p3, p4])
-third_group = first_group + second_group
-
-print(len(first_group))
-print(second_group)
-print(third_group[0])
-
-for person in third_group:
-    print(person)
 
 import unittest
+
 
 class TestPerson(unittest.TestCase):
     def setUp(self):
@@ -56,7 +61,7 @@ class TestPerson(unittest.TestCase):
     def test_custom_repr(self):
         result = repr(self.person_1)
         self.assertIn("Stefan", result)
-        self.assertEqual(result, "Person 0: Stefan Dimitrov")
+        self.assertEqual(result, "Stefan Dimitrov")
 
     def test_custom_str(self):
         result = str(self.person_1)
@@ -64,11 +69,61 @@ class TestPerson(unittest.TestCase):
         self.assertNotIn("Person", result)
         self.assertEqual(result, "Stefan Dimitrov")
 
-    def test_auto_incremented_id(self):
-        person_1_id = Person.id_counter
-        person_2 = Person('2', '2')
+    def test_custom_add(self):
+        person_2 = Person("Test", "Testov")
+        person_3 = self.person_1 + person_2
+        self.assertEqual(person_3.name, "Stefan")
+        self.assertEqual(person_3.surname, "Testov")
 
-        
+    def test_set_attributes(self):
+        self.assertEqual(self.person_1.name, "Stefan")
+        self.assertEqual(self.person_1.surname, "Dimitrov")
+
+
+class TestGroup(unittest.TestCase):
+    def setUp(self):
+        self.person_1 = Person("Stefan", "Dimitrov")
+        self.person_2 = Person("Second", "Person")
+        self.group = Group("test", [self.person_1, self.person_2])
+
+    def test_custom_len(self):
+        result = len(self.group)
+        self.assertEqual(result, 2)
+
+    def test_custom_add(self):
+        person_3 = Person("Third", "Third")
+        group_2 = Group("test2", [person_3])
+        group_3 = self.group + group_2
+        self.assertEqual(len(group_3), 3)
+
+    def test_custom_get_item(self):
+        result = self.group[1]
+        self.assertIn("Second", result)
+        self.assertEqual(result,"Person 1: Second Person")
+
+
+    def test_custom_get_item_invalid_index(self):
+        with self.assertRaises(IndexError):
+            result = self.group[2]
+
+    def test_custom_repr(self):
+        result = repr(self.group)
+        # self.assertIn("Group", result)
+        # self.assertIn("Stefan", result)
+        # self.assertIn("Second", result)
+        self.assertEqual(result, "Group test with members Stefan Dimitrov, Second Person")
+
+    def test_custom_str(self):
+        result = str(self.group)
+        # self.assertIn("Group", result)
+        # self.assertIn("Stefan", result)
+        # self.assertIn("Second", result)
+        self.assertEqual(result, "Group test with members Stefan Dimitrov, Second Person")
+
+    def test_set_attributes(self):
+        self.assertEqual(self.group.name, "test")
+        self.assertEqual(len(self.group.people), 2)
+
 
 if __name__ == "__main__":
     unittest.main()
--- Advanced level/Advanced OOP/06. Polymorphism and Magic Methods - Lab + Ex/07.Account.py
+++ Advanced level/Advanced OOP/06. Polymorphism and Magic Methods - Lab + Ex/07.Account.py
@@ -64,28 +64,107 @@ class Account:
         return acc
 
 
+#
+# acc = Account('bob', 10)
+# acc2 = Account('john')
+# print(acc)
+# print(repr(acc))
+# acc.add_transaction(20)
+# acc.add_transaction(-20)
+# acc.add_transaction(30)
+# print(acc.balance)
+# print(len(acc))
+# for transaction in acc:
+#     print(transaction)
+# print(acc[1])
+# print(list(reversed(acc)))
+# acc2.add_transaction(10)
+# acc2.add_transaction(60)
+# print(acc > acc2)
+# print(acc >= acc2)
+# print(acc < acc2)
+# print(acc <= acc2)
+# print(acc == acc2)
+# print(acc != acc2)
+# acc3 = acc + acc2
+# print(acc3)
+# print(acc3._transactions)
 
-acc = Account('bob', 10)
-acc2 = Account('john')
-print(acc)
-print(repr(acc))
-acc.add_transaction(20)
-acc.add_transaction(-20)
-acc.add_transaction(30)
-print(acc.balance)
-print(len(acc))
-for transaction in acc:
-    print(transaction)
-print(acc[1])
-print(list(reversed(acc)))
-acc2.add_transaction(10)
-acc2.add_transaction(60)
-print(acc > acc2)
-print(acc >= acc2)
-print(acc < acc2)
-print(acc <= acc2)
-print(acc == acc2)
-print(acc != acc2)
-acc3 = acc + acc2
-print(acc3)
-print(acc3._transactions)
+
+import unittest
+import types
+
+class TestAccount(unittest.TestCase):
+    def setUp(self):
+        self.account = Account("Stefan", 50)
+        self.account_other = Account("Ivan", 100)
+
+    def test_validation_tr(self):
+        res = Account.validate_transaction(self.account, 50)
+        self.assertEqual(res, "New balance: 100")
+
+    def test_validate_tr_invalid(self):
+        with self.assertRaises(ValueError) as ex:
+            res = Account.validate_transaction(self.account, -51)
+
+    def test_str(self):
+        self.assertEqual(str(self.account), "Account of Stefan with starting amount: 50")
+
+    def test_repr(self):
+        self.assertEqual(repr(self.account), "Account(Stefan, 50)")
+
+    def test_add_transaction(self):
+        self.assertEqual(len(self.account._transactions), 0)
+        self.account.add_transaction(20)
+        self.assertEqual(len(self.account._transactions), 1)
+        self.assertEqual(self.account.balance, 70)
+
+    def test_add_transaction_invalid_type(self):
+        with self.assertRaises(ValueError) as ex:
+            self.account.add_transaction(5.6)
+
+    def test_get_item(self):
+        self.account.add_transaction(20)
+        self.assertEqual(self.account[0], 20)
+
+    def test_raise_error_index(self):
+        with self.assertRaises(IndexError) as ex:
+            self.assertEqual(self.account[0], 20)
+
+    def test_reversce(self):
+        self.account.add_transaction(50)
+        self.account.add_transaction(100)
+        self.assertEqual(list(reversed(self.account)), [100, 50])
+
+    def test_are_not_equal(self):
+        self.assertNotEqual(self.account, self.account_other)
+
+    def test_less_than(self):
+        self.assertLess(self.account, self.account_other)
+
+    def test_greater_than(self):
+        self.assertGreater(self.account_other, self.account)
+        self.assertTrue(self.account_other > self.account)
+
+    def test_add(self):
+        res = self.account + self.account_other
+        self.assertEqual(res.balance, 150)
+
+    def test_len_account(self):
+        self.account.add_transaction(20)
+        self.account.add_transaction(50)
+        self.assertEqual(len(self.account), 2)
+
+    def test_g_e(self):
+        ac3 = Account("Other", 50)
+        self.assertEqual(self.account, ac3)
+
+    def test_if_validate_is_static(self):
+        self.assertTrue(isinstance(self.account.validate_transaction, types.FunctionType))
+
+    def test_greater_or_equal(self):
+        ac3 = Account("Other", 50)
+        self.assertGreaterEqual(ac3, self.account)
+
+if __name__ == "__main__":
+    unittest.main()
