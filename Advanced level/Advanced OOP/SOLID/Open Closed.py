610a8441f6fa057061b12fbcfb07c207290b5e78
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-08-25 10:49:30 +0300

    Open Closed

--- Advanced level/Advanced OOP/SOLID/Open Closed.py
+++ Advanced level/Advanced OOP/SOLID/Open Closed.py
@@ -11,7 +11,46 @@ or if we want to change the behavior of the class we shouldn't change any other 
         - Generic functions (using overloading)
 
 '''
+# Violation
+from abc import ABC, abstractmethod
+
+'''
+If we want to extend our behavior we have to modify the get_discount method adding additional if.
+we can resolve this problem using abstraction
+'''
+
 
 class StudentTaxes:
-    def __init__(self,name, semester_fee,average_grade):
+    def __init__(self, name, semester_fee, average_grade):
+        self.name = name
+        self.semester_fee = semester_fee
+        self.average_grade = average_grade
+
+    def get_discount(self):
+        if self.average_grade >= 5:
+            return self.semester_fee * 0.4
+        elif self.average_grade >= 4:
+            return self.semester_fee * 0.3
+
+
+class StudentTaxes1(ABC):
+    def __init__(self, name, semester_fee, average_grade):
+        self.name = name
+        self.semester_fee = semester_fee
+        self.average_grade = average_grade
+
+    @abstractmethod
+    def get_discount(self):
         pass
+
+
+class ExcellentStudent(StudentTaxes1):
+
+    def get_discount(self):
+        return self.semester_fee * 0.4
+
+
+class GoodStudent(StudentTaxes1):
+
+    def get_discount(self):
+        return self.semester_fee * 0.3
